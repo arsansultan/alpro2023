5ba4118d636f93a3adf7bde3cb907ea55d4b1db3
arsansultan <arsanlaode593@gmail.com>
2023-10-15 17:55:37 +0800

    tugas 1 dan 2 selesai

---
+++ tugas-1-2/tugas2.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+	x = ("Apel", "Belimbing", "Ceri")
+	y = ["Apel", "Belimbing", "Ceri"]
+	z = {"a": "Apel", "b": "Belimbing", "c": "Ceri"}
+	print(type(x))
+	print(x)
+	print(type(y))
+	print(y)
+	print(type(z))
+	print(z)
